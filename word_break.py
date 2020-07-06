0ebc73e9b7ddcf7b24adf9709f09266cac64861d
Allen Shaw <lonsty@sina.com>
2020-07-06 19:55:07 +0800

    Update word_break.py

--- word_break.py
+++ word_break.py
@@ -2,12 +2,12 @@ n = int(input().strip())
 words = [input().strip() for _ in range(n)]
 word = input().strip()
 
-def word_break(wordList, word): 
+def word_break(word_list, word): 
     if word == '': 
         return True
     else: 
-        wordLen = len(word) 
-        return any([(word[:i] in wordList) and wordBreak(wordList, word[i:]) for i in range(1, wordLen+1)]) 
+        word_len = len(word) 
+        return any([(word[:i] in word_list) and word_break(word_list, word[i:]) for i in range(1, word_len + 1)]) 
 
 if word_break(words, word):
     print('yes')
