e398a20ef7c619820898ea630a73b940472c4642
Allen Shaw <lonsty@sina.com>
2020-07-08 08:19:29 +0800

    Update word_break.py

--- word_break.py
+++ word_break.py
@@ -1,3 +1,21 @@
+"""
+题目描述
+判断所给的字符串是否由所给的词典中的若干个词组成。
+
+如已知词典["code", "sangfor", "org"]
+则字符串"codesangfororg" 由上述词典组成，
+字符串"codesangforsangfororg" 也由上述词典组成，
+但字符串"sangforcom" 则不由上述词典组成。
+
+输入描述:
+第一行一个数字K 表示词典个数
+后面若干行则为具体的输入词典，一个词典一行
+最后一行输入待判定的字符串
+
+输出描述:
+若字符串为对应的词典组成，则输出yes，否则输出no
+"""
+
 n = int(input().strip())
 words = [input().strip() for _ in range(n)]
 word = input().strip()
